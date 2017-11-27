71b93a402cad71d5e42c2d015ada4f0c96b7aed9
Greivin López <greivin.lopez@gmail.com>
2017-11-27 14:42:26 -0600

    Update akita.go
    Updating year on the MIT license.

--- akita.go
+++ akita.go
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 Greivin López
+// Copyright (c) 2017 Greivin López
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
