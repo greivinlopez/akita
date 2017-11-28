83b4791e56f0c5e3d20b69db948155ef1094ba7a
Greivin López <greivin.lopez@gmail.com>
2017-11-28 17:11:29 -0600

    Add HTML parser to retrieve links from pages and test broken links recursively

--- akita.go
+++ akita.go
@@ -21,42 +21,102 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"net/http"
+	"net/url"
+
 	"github.com/fatih/color"
-	"github.com/smallnest/goreq"
-	"time"
+	"github.com/jackdanger/collectlinks"
+	"gopkg.in/fatih/set.v0"
 )
 
 const (
-	versionNumber    = "1.0"
+	versionNumber = "1.01"
 )
 
 var (
-	website    = ""
-	version   = false
+	website = ""
+	version = false
+
+	// Color objects for output printing
+	green *color.Color
+	red   *color.Color
+
+	// Set to avoid infinite loops
+	s *set.Set
+
+	// Http client
+	client http.Client
 )
 
-func crawl(url string) {
-    // Creating http client
-	var request *goreq.GoReq
-	request = goreq.New().Timeout(7 * time.Second)
+func fixUrl(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
+}
 
-    green := color.New(color.FgGreen).SprintFunc()
-	red := color.New(color.FgRed).SprintFunc()
+func crawl(uri string) {
+	printGreen := green.SprintFunc()
+	printRed := red.SprintFunc()
 
-    resp, _, _ := request.Get(url).EndBytes()
+	resp, err := client.Get(uri)
+	if err != nil {
+		fmt.Printf("Testing %s -> %s\n", uri, printRed("Fail: "+err.Error()))
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		fmt.Printf("Testing %s -> %s\n", uri, printGreen("OK"))
+	} else {
+		fmt.Printf("Testing %s -> %s\n", uri, printRed("Fail: "+resp.Status))
+	}
 
-    if resp.StatusCode == 200 {
-        fmt.Printf("Testing %s -> %s\n", url, green("OK"))
-    } else {
-        fmt.Printf("Testing %s -> %s\n", url, red("Fail: "+resp.Status))
-    }
+	s.Add(uri)
+
+	links := collectlinks.All(resp.Body)
+	//fmt.Printf("Links: %v", links)
+
+	for _, link := range links {
+		absolute := fixUrl(link, uri)
+		if !s.Has(absolute) && uri != "" {
+			u, err := url.Parse(absolute)
+			if err != nil {
+				panic(err)
+			}
+			if u.Host == "www.westernasset.com" {
+				crawl(absolute)
+			}
+		}
+	}
 }
 
 func init() {
+	// Command line parameters
 	flag.BoolVar(&version, "version", false, "Use this flag if you want to verify the version of the tool.")
-	flag.StringVar(&website, "website", "http://www.westernasset.com/us/en/", "Website url or host: http://www.westernasset.com/us/en/.")
+	flag.StringVar(&website, "website", "", "Website url or host: http://www.google.com")
+
+	// Color objects for output printing
+	green = color.New(color.FgGreen)
+	red = color.New(color.FgRed)
+
+	// Creating http client
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client = http.Client{Transport: transport}
+
+	// Initialize our thread safe Set
+	s = set.New()
 }
 
 func main() {
@@ -65,6 +125,11 @@ func main() {
 	if version {
 		fmt.Println("akita version " + versionNumber)
 	} else {
-		crawl(website)
+		if website != "" {
+			// Start crawling the website from the given root URL
+			crawl(website)
+		} else {
+			fmt.Println("Please provide website: ./akita -website=\"http://www.google.com\" ")
+		}
 	}
 }
